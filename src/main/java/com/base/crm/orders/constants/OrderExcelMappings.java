bfe9d53baa6609209cf93da25640cea6e0cde65b
zhanglongjack <364666387@qq.com>
2018-12-23 16:31:31 +0800

    修改订单报表

--- src/main/java/com/base/crm/orders/constants/OrderExcelMappings.java
+++ src/main/java/com/base/crm/orders/constants/OrderExcelMappings.java
@@ -1,37 +1,27 @@
 package com.base.crm.orders.constants;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.base.common.util.ExcelMappingsAbstract;
-import com.base.crm.common.constants.OrderStatus;
-import com.base.crm.common.constants.PaymentMethodStatus;
 import com.base.crm.orders.entity.CustOrder;
-import com.base.crm.users.constants.UserContainer;
 
 @Component
 public class OrderExcelMappings extends ExcelMappingsAbstract {
-	private static final Logger logger = LoggerFactory.getLogger(OrderExcelMappings.class);
+//	private static final Logger logger = LoggerFactory.getLogger(OrderExcelMappings.class);
 	private static String sheetName ="订单信息";
 	
 	private static List<String> headerList = Arrays.asList(
-			"订单编号","快递公司","快递单号","快递费","客服名称","联系人","电话","地址","产品列表","付款方式",
-			"定金","到付金额","总金额","快递手续费",
-			"下单日期","订单状态");
+			"订单编号","快递公司","快递单号","快递费","联系人","电话","地址","产品列表","付款方式",
+			"定金","到付金额","总金额","实收金额","快递手续费",
+			"下单日期","订单状态","客服名称");
 	
 	public static List<String> columnsMappings = Arrays.asList(
-			"orderNo","expressCompany","expressNo","expressAmount","userId","contact","oPhone","address","productList","paymentMethod",
-			"deposits","cashOnDeliveryAmt","totalAmt","commission",
-			"orderDate","orderStatus");
+			"orderNo","expressCompany","expressNo","expressAmount","contact","oPhone","address","productList","paymentMethod",
+			"deposits","cashOnDeliveryAmt","totalAmt","payAmount","commission",
+			"orderDate","orderStatus","userId");
 	
 	public void setOrderExcelMappings(List<CustOrder> data) {
 		this.data = JSON.parseArray(JSON.toJSONString(data));
